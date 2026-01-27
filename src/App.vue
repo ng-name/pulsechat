f4d5d05ecaa2a3a64b7cb313c5b91f20ebb06316
ng-name <1148892079@qq.com>
2026-01-27 11:02:12 +0800

    feat: 完成项目初始化并配置 Tailwind CSS 3.4

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+   <div class="m-10 p-10 bg-purple-600 text-white text-3xl font-bold underline rounded-xl shadow-2xl">
+    Tailwind 测试
+  </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
