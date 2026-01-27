0e1451c01ef641c5cf26ea682ef4610a7eb01ac1
ng-name <1148892079@qq.com>
2026-01-27 11:39:34 +0800

    add shadcn-vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
 </script>
 
 <template>
-  <div>
-   <div class="m-10 p-10 bg-purple-600 text-white text-3xl font-bold underline rounded-xl shadow-2xl">
-    Tailwind 测试
-  </div>
-  </div>
+  <SidebarProvider>
+           <div class="m-10 p-10 bg-purple-600 text-white text-3xl font-bold underline rounded-xl shadow-2xl">
+        Tailwind 测试 123
+      </div>
+    <SidebarInset>
+      <header class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+         Tailwind head
+      </header>
+    </SidebarInset>
+  </SidebarProvider>
 </template>
 
 <style scoped>
