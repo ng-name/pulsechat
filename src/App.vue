418b8bdd96f97b40d01e2b148bde64dcd0511ee0
ng-name <1148892079@qq.com>
2026-01-27 14:12:25 +0800

    add shadcn left-in

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,51 @@
 <script setup lang="ts">
+import { useDark } from '@vueuse/core'
+import AppSidebar from "@/components/AppSidebar.vue"
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
+import { Separator } from "@/components/ui/separator"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
+const isDark = useDark()
 </script>
 
 <template>
-  <SidebarProvider>
-           <div class="m-10 p-10 bg-purple-600 text-white text-3xl font-bold underline rounded-xl shadow-2xl">
-        Tailwind 测试 123
-      </div>
+  <SidebarProvider :theme="isDark ? 'dark' : 'light'">
+    <AppSidebar />
     <SidebarInset>
       <header class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-         Tailwind head
+         
+        <div class="flex items-center gap-2 px-4">
+          <SidebarTrigger class="-ml-1" />
+          <Separator
+            orientation="vertical"
+            class="mr-1 data-[orientation=vertical]:h-4"
+          />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem class="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator class="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+
       </header>
     </SidebarInset>
   </SidebarProvider>
---
+++ src/components/TeamSwitcher.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type { Component } from "vue"
+
+import { ChevronsUpDown, House } from "lucide-vue-next"
+import { ref } from "vue"
+
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar"
+
+const props = defineProps<{
+  teams: {
+    name: string
+    logo: Component
+    plan: string
+  }[]
+}>()
+
+const activeTeam = ref(props.teams[0])
+</script>
+
+<template>
+  <SidebarMenu>
+    <SidebarMenuItem>
+      <SidebarMenuButton
+            size="lg"
+            class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+          >
+            <div class="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+              <component :is="House" class="size-4" />
+            </div>
+            <div class="grid flex-1 text-left text-sm leading-tight">
+              <span class="truncate font-medium">
+                PulseChat
+              </span>
+              <span class="truncate text-xs">
+                epxins
+              </span>
+            </div>
+            <House class="ml-auto" />
+          </SidebarMenuButton>
+    </SidebarMenuItem>
+  </SidebarMenu>
+</template>
